1d806a36e6e3d0b4332dd66c47cb9e1d4e704bac
Rayyan1222 <156992065+Rayyan1222@users.noreply.github.com>
2024-04-09 14:52:54 -0500

    Update js

---
+++ js/slider.js
@@ -0,0 +1,34 @@
+let slideIndex = 0;
+
+function showSlides() {
+  const slides = document.querySelectorAll('#slider .slides img');
+  const totalSlides = slides.length;
+
+  for (let i = 0; i < totalSlides; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  if (slideIndex >= totalSlides) {
+    slideIndex = 0;
+  } else if (slideIndex < 0) {
+    slideIndex = totalSlides - 1;
+  }
+
+  slides[slideIndex].style.display = 'block';
+}
+
+function nextSlide() {
+  slideIndex++;
+  showSlides();
+}
+
+function prevSlide() {
+  slideIndex--;
+  showSlides();
+}
+
+// Automatically advance slides
+setInterval(nextSlide, 3000);
+
+// Initial display
+showSlides();
